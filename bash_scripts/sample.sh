e38a8d067d53c13fd61accb185e466f7fefca911
Giang Vu Long <longgiang2015bn@gmail.com>
2023-03-28 11:39:33 +0700

    new sample scripts

--- bash_scripts/sample.sh
+++ bash_scripts/sample.sh
@@ -2,8 +2,8 @@
 
 export MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --dropout 0.1"
 export DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear"
-export TRAIN_FLAGS="--lr 1e-4 --batch_size 128 --save_interval 50000"
-export OPENAI_LOGDIR="log"
+export SAMPLE_FLAGS="--num_samples 50000 --batch_size 128 --model_path log/ema_0.9999_350000.pt"
+export OPENAI_LOGDIR="sample_log"
 export OPENAI_LOG_FORMAT="csv"
 
-python scripts/image_train.py --data_dir cifar_train $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
+python scripts/image_sample.py $MODEL_FLAGS $DIFFUSION_FLAGS $SAMPLE_FLAGS
