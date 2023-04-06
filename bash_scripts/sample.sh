7832218b12e1f43ea9f3b5ea94a97360412d3d52
Giang Vu Long <longgiang2015bn@gmail.com>
2023-04-07 00:08:47 +0700

    feat: residual block done

--- bash_scripts/sample.sh
+++ bash_scripts/sample.sh
@@ -4,6 +4,6 @@ export MODEL_FLAGS="--image_size 32 --num_channels 128 --num_res_blocks 3 --drop
 export DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear"
 export SAMPLE_FLAGS="--num_samples 50000 --batch_size 128 --model_path log/ema_0.9999_350000.pt"
 export OPENAI_LOGDIR="sample_log"
-export OPENAI_LOG_FORMAT="csv"
+export OPENAI_LOG_FORMAT="stdout"
 
 python scripts/image_sample.py $MODEL_FLAGS $DIFFUSION_FLAGS $SAMPLE_FLAGS
